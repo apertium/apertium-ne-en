b5cf3be846c4f8b4307723269d5ef46030ebf124
aishraj <aishraj@oqaa.projectjj.com>
2011-06-25 07:46:08 +0000

    The test reults for 1000 random entries

---
+++ dev/tests/test_random.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<cstdlib>
+#include<cstdio>
+#include<fstream>
+#include<string>
+using namespace std;
+int limited_rand(int limit)
+{
+  int r, d = RAND_MAX / limit;
+  limit *= d;
+  do { r = rand(); } while (r >= limit);
+  return r / d;
+}
+int main()
+{ 
+  vector<string> vs;
+  vector<int> v;
+  string line;
+  string myinput;
+  string brk="\n";
+  string com="\"";
+  string ltp=" | lt-proc ne-en.automorf.bin";
+  ifstream myfile ("hitparade_stripped.txt");
+  if (myfile.is_open())
+  {
+    while (myfile.good())
+    {
+      getline (myfile,line);
+      string mycm=com+line+com;
+      //cout<<line<<endl;
+      myinput= "echo "+mycm+ltp+brk;
+      //system(myinput);
+      //Changes made here
+     // cout<<myinput;
+     vs.push_back(myinput);
+    }
+    myfile.close();
+  }
+  else 
+    cout<<"Unable to open the file"<<endl;
+  
+  for(int i=0;i<=999;i++)
+  {
+    v.push_back(limited_rand(330272));
+  }
+  //for(int i=0;i<=999;i++)
+    //cout<<vs[v[i]];
+    for(int j=0;j<=999;j++)
+    {
+      string t;
+      t=vs[v[j]];
+      const char* c= t.c_str();
+      system(c);
+    }
+  return 0;
+}
